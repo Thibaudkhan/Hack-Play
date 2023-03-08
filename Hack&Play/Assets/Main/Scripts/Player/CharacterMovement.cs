a129c02b497e4fa7cac3f3310e85c2ac4a58ea7b
Thib <thibaudroussel199995@gmail.com>
2023-03-08 19:55:36 +0100

    add Player skin and some annimations

--- Hack&Play/Assets/Main/Scripts/Player/CharacterMovement.cs
+++ Hack&Play/Assets/Main/Scripts/Player/CharacterMovement.cs
@@ -7,7 +7,13 @@ public class CharacterMovement : MonoBehaviour
 {
     public CharacterController controller;
     
+    [SerializeField]
     private float speed = 6f;
+    private float sprintSpeed = 7f; // new variable for sprint speed
+    private bool isDrunk = true;
+    
+    private Animator animator;
+    
     private float gravity = -9.81f;
     private float jumpHeight = 2f;
     
@@ -18,6 +24,13 @@ public class CharacterMovement : MonoBehaviour
     Vector3 velocity;
     bool isGrounded;
 
+    
+    void Start()
+    {
+        //animator = GetComponentInChildren<Animator>(true);    
+        animator = GetComponent<Animator>();
+    }
+    
     private void Update()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
@@ -32,21 +45,54 @@ public class CharacterMovement : MonoBehaviour
         
         Vector3 move = transform.right * x + transform.forward * z;
         
-        controller.Move(move * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.LeftShift)) // check if Left Shift is pressed
+        {
+            move *= sprintSpeed; // if yes, use sprint speed
+        }
+        else
+        {
+            move  *= speed; // if not, use regular speed
+        }
+        
+        if(move != Vector3.zero)
+        {
+            if (isDrunk)
+            {
+                animator.SetBool("isMovingDrunk", true);
+
+            }
+            else
+            {
+                animator.SetBool("isMoving", true);
+
+            }
+            
+        }
+        else
+        {
+            if (isDrunk)
+            {
+                animator.SetBool("isMovingDrunk", false);
+
+            }
+            else
+            {
+                animator.SetBool("isMoving", false);
+
+            }
+        }
+        
+        controller.Move(move *  Time.deltaTime);
 
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             Debug.Log("Jump");
-            velocity.y = Mathf.Sqrt( -2f * gravity);
+            animator.SetBool("Jumping", true);
 
-        }
-        // if (Input.GetButtonDown("Iteraction") && isGrounded)
-        // {
-        //     Debug.Log("computer");
-        //
-        //     
-        // }
+            velocity.y = Mathf.Sqrt( -2f * gravity);
         
+        }
+
         velocity.y += gravity * Time.deltaTime;
         
         controller.Move(velocity * Time.deltaTime);
